6a97d594d62af65a3bdb02c8df673efbd75dace2
MENO Daisuke <menonon1@gmail.com>
2013-07-10 14:54:53 +0900

    several files added

--- src/JobSchedulerAbstractionSystem.py
+++ src/JobSchedulerAbstractionSystem.py
@@ -1,8 +1,16 @@
+import os
+import fnmatch
+import json
+import fileinput
+
 class JobSchedulerAbstractionSystem:
-    def __init__(self, scheduler='', prefix='', shell='/bin/sh', queue='', group='', elapse='00:15:00', MPI=True, node='1', process='1', OMP=True, thread='0', run='a.out'):
+    def __init__(self, scheduler='', shell='/bin/sh', err2stdout=True, \
+                 queue='', group='', elapse='00:15:00', \
+                 MPI=True, node='1', process='1', \
+                 OMP=True, thread='0', run='a.out'):
         self.scheduler = scheduler
-        self.prefix = prefix
         self.shell = shell
+        self.err2stdout = err2stdout
         self.queue = queue
         self.group = group
         self.elapse = elapse
@@ -16,7 +24,14 @@ class JobSchedulerAbstractionSystem:
         
         self.run = run
 
-    def readFormat():
-    
-    def outputScript():
-    
+class JobSchedulerDatabase:
+    def __init__(self):
+        schdlSetting = ''
+        self.schdl = list()
+        for file in os.listdir('.'):
+            if fnmatch.fnmatch(file, 'schdl_*.json'):
+                schdlSetting=''
+                for line in fileinput.input(file):
+                    schdlSetting += line
+                print schdlSetting
+                self.schdl.append(json.loads(schdlSetting))
